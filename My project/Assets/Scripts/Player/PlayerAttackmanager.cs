6afd04177f27d86c4ef266b9bebaa7bd09b5d3e4
Rojehag <jenz.rojehag@icloud.com>
2026-05-20 13:01:10 +0200

    Jobbat med animations

---
+++ My project/Assets/Scripts/Player/PlayerAttackmanager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerAttackmanager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Attack");
+        }
+    }
+}
--- My project/Assets/Scripts/Player/PlayerMovment.cs
+++ My project/Assets/Scripts/Player/PlayerMovment.cs
@@ -17,6 +17,9 @@ public class PlayerMovment : MonoBehaviour
     [Header("Particle System")]
     [SerializeField]ParticleSystem particleSystem;
 
+    [Header("Animation")]
+    [SerializeField] Animator playerAnim;
+
 
     //Player direction to check the direction of the player
     Vector2 playerDirection;
@@ -35,16 +38,16 @@ public class PlayerMovment : MonoBehaviour
         //Player movement and jump
         if (Input.GetKey(KeyCode.A))
         {
-            Movment(new Vector2(-playerSpeed, playerRb.linearVelocity.y),0);
+            Movment(new Vector2(-playerSpeed, playerRb.linearVelocity.y),0,180, true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Movment(new Vector2(playerSpeed, playerRb.linearVelocity.y), -172);
+            Movment(new Vector2(playerSpeed, playerRb.linearVelocity.y), -172,0, true);
         }
         else
         {
             particleSystem.Stop();
-            Movment(new Vector2(0, playerRb.linearVelocityY),0);
+            Movment(new Vector2(0, playerRb.linearVelocityY),100,100, false);
         }
         if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Physics>().CheckIfGrounded())
         {
@@ -55,13 +58,33 @@ public class PlayerMovment : MonoBehaviour
     }
 
     //Function to move the player in the direction of the input and speed
-    void Movment(Vector2 direction, int rotation)
+    void Movment(Vector2 direction, int rotation, int rotationZ , bool animation)
     {
-        if (GetComponent<Physics>().CheckIfGrounded() && particleSystem.isStopped) 
+        if (GetComponent<Physics>().CheckIfGrounded()&& particleSystem.isStopped) 
         {
             particleSystem.Play();
+            
         }
-        particleSystem.transform.rotation = Quaternion.Euler(0, rotation, 0);
+        else if (!GetComponent<Physics>().CheckIfGrounded() && particleSystem.isPlaying)
+        {
+            particleSystem.Stop();
+            playerAnim.SetBool("PlayerRun", false);
+        }
+        if(rotationZ!= 100)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, rotationZ, 0);
+            particleSystem.transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+
+        if(animation)
+        {
+            playerAnim.SetBool("PlayerRun", true);
+        }else
+        {
+            playerAnim.SetBool("PlayerRun", false);
+        }
+
+        
         playerRb.linearVelocity = new(Mathf.Lerp(playerRb.linearVelocityX, direction.x, Time.deltaTime * acceleration), direction.y);
     }
 
